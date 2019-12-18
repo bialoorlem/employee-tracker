346b7162b19df83ec742788eaa0adf635a4e1a40
Elliot Trejo <elliot.trejo.tcu@gmail.com>
2019-12-18 17:22:42 -0600

    Received help from Patrick, added schema and modified seed.sql from Patrick.

--- db/seed.sql
+++ db/seed.sql
@@ -1,27 +1,8 @@
-USE ee_info_db;
-INSERT INTO departments (name)
-VALUES ('Accounts Receivable');
-INSERT INTO departments (name)
-VALUES ('HR');
-INSERT INTO departments (name)
-VALUES ('Operations');
-INSERT INTO departments (name)
-VALUES ('R&D');
-INSERT INTO departments (name)
-VALUES ('Strawberry');
-INSERT INTO roles (title, salary, department_id)
-VALUES ("Employee",1000000, 5);
-INSERT INTO roles (title, salary, department_id)
-VALUES ("Liason",900000, 2);
-INSERT INTO roles (title, salary, department_id)
-VALUES ("Manager",20000, 1);
-INSERT INTO roles (title, salary, department_id)
-VALUES ("Vice President of Interdepartmental Communications and Corporate Synergy",2000000, 5);
-INSERT INTO employees (first_name , last_name , role_id, manager_id)
-VALUES ("Bill", "Joeman", 3, NULL);
-INSERT INTO employees (first_name , last_name , role_id, manager_id)
-VALUES ("Paul", "Hendrickson", 4, 1);
-INSERT INTO employees (first_name , last_name , role_id, manager_id)
-VALUES ("Billy", "Jean", 2, 1);
-INSERT INTO employees (first_name , last_name , role_id, manager_id)
-VALUES ("Frank", "Frankinson", 1, 1);
+INSERT INTO department (name)
+VALUES ("Human Resources"), ("R&D"), ("Engineering"), ("Accounting"), ("Sales");
+
+INSERT INTO role (title, salary, department_id)
+VALUE ("manager", 75000.00, 2), ("engineer", 52000, 3), ("accountant", 62500, 4), ("recruiter", 57500, 1), ("sales person", 85650, 5);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUE ("John", "Smith", 1, null), ("Jill", "Green", 1, 1), ("Jack", "Brown", 3, 2), ("Steve", "Quinn", 5, 2);
